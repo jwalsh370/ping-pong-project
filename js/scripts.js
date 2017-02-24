1e1a5263d7d8a334ebb800bdfa1e3e8ce99ab15d
jahan <jwalsh370@gmail.com>
2017-02-24 14:23:26 -0800

    makes js code dry and easily readable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-var ping = 3;
-var pong = 5;
-var pingPong = 15;
 var userInput;
 var result = [];
 //Backend
@@ -8,42 +5,20 @@ $(function(){
   $("#form").submit(function(event){
       event.preventDefault();
 
-    var userInput = $("input#ping").val();
-    console.log(userInput);
+  var userInput = $("input#ping").val();
 
-
-  for(var n = 1; n <= userInput; n++){
-    if((n % 15) === 0){
+  for(var j = 1; j <= userInput; j++){
+    if((j % 15) === 0){
       result.push('Ping Pong');
-    }else if((n % 5) === 0){
+    }else if((j % 5) === 0){
       result.push('Pong');
-    }else if((n %3) === 0){
+    }else if((j %3) === 0){
       result.push('Ping');
     }else{
-      result.push(n)
-      console.log(result);
-
+      result.push(j)
     }
   }
-
   // front end
-
-
   $("#outputResult").append(result.join("<br>"));
-
-
-
-
 });
-});
-$(function() {
-
-function codeLeft() {
-    $("#move").animate({left: "-=300"}, 600, "swing", codeRight);
-}
-function codeRight() {
-    $("#move").animate({left: "+=300"}, 600, "swing", codeLeft);
-}
-
-codeRight();
 });
